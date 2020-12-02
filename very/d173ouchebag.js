7e38d598e56c53366dd2c0a7b8f339498de06895
ambrdroid <75393220+ambrdroid@users.noreply.github.com>
2020-12-02 17:36:03 -0600

    Update d173ouchebag.js

--- very/d173ouchebag.js
+++ very/d173ouchebag.js
@@ -1,4 +1,4 @@
-var NumberOfWords = 5
+var NumberOfWords = 10
 var words = new BuildArray(NumberOfWords)
 
 // Use the following variables to 
@@ -7,7 +7,12 @@ words[1] = "http://gestyy.com/erM0Ow"
 words[2] = "http://gestyy.com/erM0Dr" 
 words[3] = "http://gestyy.com/erM0J9" 
 words[4] = "http://gestyy.com/erM0Vr" 
-words[5] = "http://gestyy.com/erM04t"
+words[5] = "http://gestyy.com/erM04t" 
+words[6] = "http://gestyy.com/erMqtU" 
+words[7] = "http://gestyy.com/erMqg6" 
+words[8] = "http://gestyy.com/erMqcl" 
+words[9] = "http://gestyy.com/erMqmZ" 
+words[10] = "http://gestyy.com/erMqEn" 
 
 function BuildArray(size){
 this.length = size
